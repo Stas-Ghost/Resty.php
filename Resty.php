bb9f8154d47764379aacb8ba10be7f98363f0552
Stas Yakushev <stas.ghost@gmail.com>
2015-05-18 12:48:29 +0600

    fix #8 Now working with https

--- Resty.php
+++ Resty.php
@@ -723,6 +723,10 @@ class Resty
 				'header'=>$headerarr,
 				'ignore_errors'=>1,
 				'max_redirects'=>$max_redirects
+			),
+			'ssl' => array(
+				'verify_peer'=>false,
+				'verify_peer_name'=>false
 			)
 		);
 
